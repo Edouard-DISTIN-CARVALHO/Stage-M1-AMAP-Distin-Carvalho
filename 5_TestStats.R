73e6de195efec89ec8a4b991af120cd3cc894a26
Edouard DISTIN CARVALHO <distincarvalho@cirad.fr>
2024-05-15 16:43:29 +0200

    Extraction des paramettres des modèles

---
+++ 5_TestStats.R
@@ -0,0 +1,89 @@
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  
+#                          5/ Test Statistiques                                 ----
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+setwd("C:/Users/distincarvalho/OneDrive/Documents/R/AMAP/Git")
+source("C:/Users/distincarvalho/OneDrive/Documents/R/AMAP/Git/1_Standardisation.R") 
+source("C:/Users/distincarvalho/OneDrive/Documents/R/AMAP/Git/2_Composition.R") 
+source("C:/Users/distincarvalho/OneDrive/Documents/R/AMAP/Git/3_ Saisonalite.R")
+source("C:/Users/distincarvalho/OneDrive/Documents/R/AMAP/Git/3_ Modelisation.R")
+
+##### Paramètre des modèles #### 
+
+# Total Litterfall:
+
+#Extraction des coefficients
+coef_tot_an <- coef(NLS_tot_an)
+coef_tot_bi <- coef(NLS_tot_bi)
+coef_tot_tri <- coef(NLS_tot_tri)
+coef_tot_ct_an <- coef(NLS_tot_ct_an)
+coef_tot_ct_bi <- coef(NLS_tot_ct_bi)
+coef_tot_ct_tri <- coef(NLS_tot_ct_tri)
+
+# Création d'un nouveau dataframe
+coef_tot_reg <- data.frame(
+  Modele = c("NLS_tot_an", "NLS_tot_bi", "NLS_tot_tri",
+            "NLS_tot_ct_an", "NLS_tot_ct_bi", "NLS_tot_ct_tri"),
+  a = c(coef_tot_an["a"], coef_tot_bi["a"], coef_tot_tri["a"],
+        coef_tot_ct_an["a"], coef_tot_ct_bi["a"], coef_tot_ct_tri["a"]),
+  b = c(coef_tot_an["b"], coef_tot_bi["b"], coef_tot_tri["b"],
+        coef_tot_ct_an["b"], coef_tot_ct_bi["b"], coef_tot_ct_tri["b"]),
+  c = c(coef_tot_an["c"], coef_tot_bi["c"], coef_tot_tri["c"]),
+  Base = c(coef_tot_an["Base"], coef_tot_bi["Base"], coef_tot_tri["Base"],
+           coef_tot_ct_an["Base"], coef_tot_ct_bi["Base"], coef_tot_ct_tri["Base"]),
+  p = c(coef_tot_an["p"], coef_tot_bi["p"], coef_tot_tri["p"],
+        coef_tot_ct_an["p"], coef_tot_ct_bi["p"], coef_tot_ct_tri["p"]))
+
+# Leaves 
+
+# Extraction des coefficients
+coef_lea_an <- coef(NLS_lea_an)
+coef_lea_bi <- coef(NLS_lea_bi)
+coef_lea_tri <- coef(NLS_lea_tri)
+coef_lea_ct_an <- coef(NLS_lea_ct_an)
+coef_lea_ct_bi <- coef(NLS_lea_ct_bi)
+coef_lea_ct_tri <- coef(NLS_lea_ct_tri)
+
+# Création d'un nouveau dataframe
+coef_lea_reg <- data.frame(
+  Modele = c("NLS_lea_an", "NLS_lea_bi", "NLS_lea_tri",
+             "NLS_lea_ct_an", "NLS_lea_ct_bi", "NLS_lea_ct_tri"),
+  a = c(coef_lea_an["a"], coef_lea_bi["a"], coef_lea_tri["a"],
+        coef_lea_ct_an["a"], coef_lea_ct_bi["a"], coef_lea_ct_tri["a"]),
+  b = c(coef_lea_an["b"], coef_lea_bi["b"], coef_lea_tri["b"],
+        coef_lea_ct_an["b"], coef_lea_ct_bi["b"], coef_lea_ct_tri["b"]),
+  c = c(coef_lea_an["c"], coef_lea_bi["c"], coef_lea_tri["c"]),
+  Base = c(coef_lea_an["Base"], coef_lea_bi["Base"], coef_lea_tri["Base"],
+           coef_lea_ct_an["Base"], coef_lea_ct_bi["Base"], coef_lea_ct_tri["Base"]),
+  p = c(coef_lea_an["p"], coef_lea_bi["p"], coef_lea_tri["p"],
+        coef_lea_ct_an["p"], coef_lea_ct_bi["p"], coef_lea_ct_tri["p"]))
+
+
+# Twigs 
+
+# Extraction des coeeficients : 
+coef_twg_an <- coef(NLS_twg_an)
+coef_twg_bi <- coef(NLS_twg_bi)
+coef_twg_tri <- coef(NLS_twg_tri)
+coef_twg_ct_an <- coef(NLS_twg_ct_an)
+coef_twg_ct_bi <- coef(NLS_twg_ct_bi)
+coef_twg_ct_tri <- coef(NLS_twg_ct_tri)
+
+# Création d'un nouveau dataframe
+coef_twg_reg <- data.frame(
+  Modele = c("NLS_twg_an", "NLS_twg_bi", "NLS_twg_tri",
+             "NLS_twg_ct_an", "NLS_twg_ct_bi", "NLS_twg_ct_tri"),
+  a = c(coef_twg_an["a"], coef_twg_bi["a"], coef_twg_tri["a"],
+        coef_twg_ct_an["a"], coef_twg_ct_bi["a"], coef_twg_ct_tri["a"]),
+  b = c(coef_twg_an["b"], coef_twg_bi["b"], coef_twg_tri["b"],
+        coef_twg_ct_an["b"], coef_twg_ct_bi["b"], coef_twg_ct_tri["b"]),
+  c = c(coef_twg_an["c"], coef_twg_bi["c"], coef_twg_tri["c"]),
+  Base = c(coef_twg_an["Base"], coef_twg_bi["Base"], coef_twg_tri["Base"],
+           coef_twg_ct_an["Base"], coef_twg_ct_bi["Base"], coef_twg_ct_tri["Base"]),
+  p = c(coef_twg_an["p"], coef_twg_bi["p"], coef_twg_tri["p"],
+        coef_twg_ct_an["p"], coef_twg_ct_bi["p"], coef_twg_ct_tri["p"]))
+
+
+coef_lea_reg
